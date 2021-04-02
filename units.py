2aa6cd5c8f8ecde844268e769357c228869d675a
Joxos <xujunhao61@163.com>
2021-04-02 20:41:54 +0800

    more translation of lengths

--- units.py
+++ units.py
@@ -1,2 +1,3 @@
-lengths = {"km": 1, "m": 1e3, "dm": 1e4, "cm": 1e5, "mm": 1e6}
+lengths = {"km": 1, "mile": 6.213712e-1, "m": 1e3, "ft": 3.281e3, "yd": 1.094e3,
+           "dm": 1e4, "cm": 1e5, "mm": 1e6, "μm": 1e9, "fathom": 5.468066492e2}
 mass = {"t": 1, "kg": 1e3, "b": 2.205e3, "g": 1e6}
